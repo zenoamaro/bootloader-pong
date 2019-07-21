ce73d519d097ba86c527688fca3fb7989750d162
zenoamaro <zenoamaro@gmail.com>
2019-07-21 17:43:18 +0200

    Paddle boundaries

--- src/loop.asm
+++ src/loop.asm
@@ -1,79 +1,85 @@
 loop: ;-------------------------------------------------------------------------
 
+
 update: ;-----------------------------------------------------------------------
 
-update_player:
 update_player_input:
-        in al, KEYBOARD_PORT
-        cmp al, KEY_W
+        in al, KEYBOARD_PORT            ; Read keyboard directly
+        cmp al, KEY_W                   ; Up
         je update_player_move_up
-        cmp al, KEY_S
+        cmp al, KEY_S                   ; Down
         je update_player_move_down
-        jmp update_player_move_end
-update_player_move:
+        jmp update_player_move_end      ; Still
 update_player_move_up:
-        dec word [P1Y]
+        mov ax, [P1Y]                   ; Top of paddle
+        cmp ax, 0                       ; Top of screen collision
+        je update_player_move_end
+        dec word [P1Y]                  ; Move up
         jmp update_player_move_end
 update_player_move_down:
-        inc word [P1Y]
+        mov ax, [P1Y]                   ; Top of paddle
+        add ax, P1H                     ; Add size of paddle
+        cmp ax, SCREEN_H                ; Bottom of screen collision
+        je update_player_move_end
+        inc word [P1Y]                  ; Move down
 update_player_move_end:
 
-update_ball:
-update_ball_move:
+
 update_ball_move_x:
         mov ax, [BallX]
         mov bx, [BallXS]
         add ax, bx
-        jz update_ball_move_x_invert
-        cmp ax, SCREEN_W
+        jz update_ball_move_x_invert    ; Left of screen collision
+        cmp ax, SCREEN_W                ; Right of screen collision
         jne update_ball_move_x_set
 update_ball_move_x_invert:
-        neg bx
+        neg bx                          ; Invert direction
         mov [BallXS], bx
 update_ball_move_x_set:
-        mov [BallX], ax
+        mov [BallX], ax                 ; Apply speed
 update_ball_move_x_end:
+
 update_ball_move_y:
         mov ax, [BallY]
         mov bx, [BallYS]
         add ax, bx
         jz update_ball_move_y_invert
-        cmp ax, SCREEN_H
+        cmp ax, SCREEN_H                ; Bottom of screen collision
         jne update_ball_move_y_set
 update_ball_move_y_invert:
-        neg bx
+        neg bx                          ; Invert direction
         mov [BallYS], bx
 update_ball_move_y_set:
-        mov [BallY], ax
+        mov [BallY], ax                 ; Apply speed
 update_ball_move_y_end:
-update_ball_move_end:
-update_ball_end:
 
 
 draw: ;-------------------------------------------------------------------------
-        call clear
+        call clear                      ; Clear the screen
 
 draw_p1:
-        mov dx, P1H
+        mov dx, P1H                     ; Size of paddle
         mov bx, P1X
         mov cl, P1C
 draw_p1_loop:
-        mov ax, [P1Y]
+        mov ax, [P1Y]                   ; Top of paddle
         add ax, dx
+        dec ax                          ; Inclusive zero loop
         call plot
         dec dx                          ; FIXME: Use loop
-        jnz draw_p1_loop
+        jnz draw_p1_loop                ; Loop to size of paddle
 
 draw_p2:
-        mov dx, P2H
+        mov dx, P2H                     ; Size of paddle
         mov bx, P2X
         mov cl, P2C
 draw_p2_loop:
-        mov ax, [P2Y]
+        mov ax, [P2Y]                   ; Top of paddle
         add ax, dx
+        dec ax                          ; Inclusive zero loop
         call plot
         dec dx                          ; FIXME: Use loop
-        jnz draw_p2_loop
+        jnz draw_p2_loop                ; Loop to size of paddle
 
 draw_ball:
         mov ax, [BallY]
@@ -83,5 +89,5 @@ draw_ball:
 
 
 spin: ;-------------------------------------------------------------------------
-        times SPINLOOP call sleep
+        times SPINLOOP call sleep       ; FIXME: Proper timestep
         jmp loop
